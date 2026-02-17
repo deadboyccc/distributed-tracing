5b795392bc599f3ffb5113e3bf21bf8e6e47bc8e
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-02-17 03:28:59 +0300

    add config server and container support

--- config-server/src/main/java/dev/dead/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/dev/dead/configserver/ConfigServerApplication.java
@@ -1,15 +1,24 @@
 package dev.dead.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ConfigServerApplication.class, args);
+
+        String repoPath = context.getEnvironment().getProperty("test.demo" +
+                ".property1");
+        log.info("Config Server started! Looking demo property: {}", repoPath);
     }
 
 }
--- service-a/src/main/java/dev/dead/servicea/ServiceAApplication.java
+++ service-a/src/main/java/dev/dead/servicea/ServiceAApplication.java
@@ -1,15 +1,22 @@
 package dev.dead.servicea;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
 public class ServiceAApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceAApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ServiceAApplication.class, args);
+
+        String repoPath = context.getEnvironment().getProperty("test.demo" +
+                ".property1");
+        log.info("repoPath: {}", repoPath);
     }
 
 }
