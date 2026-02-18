faf06a816ff5993f60b517f1b7f12be4856772d9
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-02-18 05:26:32 +0300

    add another service

---
+++ service-b/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.2"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.dead"
+version = "0.0.1-SNAPSHOT"
+description = "service-b"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.0"
+
+dependencies {
+
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
