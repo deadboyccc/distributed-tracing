bf029affc68c3de640599ac7a47df48ea1c78afb
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-02-19 05:53:55 +0300

    use observation api to log

--- service-a/src/main/java/dev/dead/servicea/ServiceAApplication.java
+++ service-a/src/main/java/dev/dead/servicea/ServiceAApplication.java
@@ -2,13 +2,21 @@ package dev.dead.servicea;
 
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.function.Supplier;
 
 @Slf4j
 @SpringBootApplication
@@ -16,29 +24,59 @@ public class ServiceAApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ServiceAApplication.class, args);
-        String repoPath = context.getEnvironment().getProperty("test.demo" +
-                ".property1");
-        String encrypted = context.getEnvironment().getProperty("test.demo.encrypted");
+        String repoPath = context.getEnvironment()
+                .getProperty("test.demo" +
+                        ".property1");
+        String encrypted = context.getEnvironment()
+                .getProperty("test.demo.encrypted");
         log.info("Config Server started! Looking demo property: {}", repoPath);
         log.info("Config Server started! Looking encrypted property: {}", encrypted);
     }
 
+    // rest client bean
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.build();
     }
 
+}
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 class ServiceAController {
     private final MyComponent myComponent;
+    private final RestClient restClient;
 
-    public ServiceAController(MyComponent myComponent) {
-        this.myComponent = myComponent;
-    }
 
     @GetMapping("/")
-    public String serviceA() {
+    public String serviceA() throws UnknownHostException {
 
+        //create span
         myComponent.doWork();
-        return "Hello from Service A!";
+        // call service B
+        ResponseEntity<String> response = restClient.get()
+                .uri("http://service-b:8081/")
+                .retrieve()
+                .toEntity(String.class); // Use toEntity() directly on the retrieve() spec
+        // creating  more spans
+        var res = myComponent.observe("serviceA.callServiceB",
+                "callServiceB", "service", "serviceA", "target", "serviceB",
+                () -> {
+                    // Call service B and return the response body
+                    ResponseEntity<String> resp = restClient.get()
+                            .uri("http://service-b:8081/span")
+                            .retrieve()
+                            .toEntity(String.class);
+                    return resp.getBody();
+                });
+        return "Hello from Service A: IP " + Inet4Address.getLocalHost()
+                .getHostAddress() +
+                " - - - - - response from service B : " +
+                " " + response.getBody()
+                + " - - - - - response from service B through observation " +
+                "span" +
+                " : " + res;
     }
 }
 
@@ -63,5 +101,19 @@ class MyComponent {
                 });
 
         log.info("Observation result: {}", y);
+    }
+
+    public <T> T observe(String observationName,
+                         String contextualName, String lowCardinalityKey,
+                         String lowCardinalityValue,
+                         String highCardinalityKey,
+                         String highCardinalityValue,
+                         Supplier<T> supplier) {
+
+        return Observation.createNotStarted(observationName, registry)
+                .contextualName(contextualName)
+                .lowCardinalityKeyValue(lowCardinalityKey, lowCardinalityValue)
+                .highCardinalityKeyValue(highCardinalityKey, highCardinalityValue)
+                .observe(supplier);
     }
 }
--- service-b/src/main/java/dev/dead/serviceb/ServiceBApplication.java
+++ service-b/src/main/java/dev/dead/serviceb/ServiceBApplication.java
@@ -2,6 +2,11 @@ package dev.dead.serviceb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class ServiceBApplication {
@@ -10,4 +15,24 @@ public class ServiceBApplication {
         SpringApplication.run(ServiceBApplication.class, args);
     }
 
+}
+
+@RestController
+class ServiceBController {
+
+    @GetMapping("/span")
+    public String span() {
+        return "Hello from Service B! This is a span endpoint.";
+    }
+
+    @GetMapping("/")
+    public String serviceB() throws UnknownHostException {
+        // + log ip through java util api
+        return "Hello from Service B!" + " Your IP is: " + getClientIp();
+    }
+
+    public String getClientIp() throws UnknownHostException {
+        return InetAddress.getLocalHost()
+                .getHostAddress();
+    }
 }
